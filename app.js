eea6c6e60dd35f8f14d914cab858eb965cb67f13
Amit-Dangi1 <amitjidangi@gmail.com>
2025-09-21 15:29:31 +0530

    cors

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ mongoose.connect(process.env.DB_URL).then(() => {
 
     app.use(express.json())
     app.use(express.urlencoded({extended:true}));
-    app.use(cors())
-
+       app.use(cors({
+  origin: "https://review-rate-frontend.onrender.com", // your frontend
+  credentials: true                 // This allows cookies to be sent
+}))
     app.use("/company", companyRouter)
     app.use("/reviews", reviewRouter)
     app.listen(3000, () => {
